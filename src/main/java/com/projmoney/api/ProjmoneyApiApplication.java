ccf7a06c2010b358a6fccb0096a66767be5ef978
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-03-07 17:15:30 -0300

    first commit

---
+++ src/main/java/com/projmoney/api/ProjmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.projmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjmoneyApiApplication.class, args);
+	}
+
+}
